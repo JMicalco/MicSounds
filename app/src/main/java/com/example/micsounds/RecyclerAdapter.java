3fdcc1e2d0bcf1fc8b14e8b700644cfed5ec05d8
José Adolfo <jmicalco@gmail.com>
2020-11-06 17:35:01 -0600

    Botones

--- app/src/main/java/com/example/micsounds/RecyclerAdapter.java
+++ app/src/main/java/com/example/micsounds/RecyclerAdapter.java
@@ -1,11 +1,16 @@
 package com.example.micsounds;
 
 import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
@@ -36,6 +41,34 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
         View view = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.population_item, parent, false);
 
+        Button btnCarrito= view.findViewById(R.id.btnCarrito);
+        btnCarrito.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(mContext, "Carrito", Toast.LENGTH_SHORT).show();
+                //Log.e("hola","hola");
+            }
+        });
+        Button btnFav= view.findViewById(R.id.btnFav);
+        btnFav.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(mContext, "Favoritos", Toast.LENGTH_SHORT).show();
+            }
+        });
+        Button btnShare= view.findViewById(R.id.btnCompartir);
+        final String shareView= "info para compartir";
+        btnShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Toast.makeText(mContext, "Compartir", Toast.LENGTH_SHORT).show();
+                Intent intent=new Intent(Intent.ACTION_SEND);
+                intent.setType("text/pain");
+                intent.putExtra(Intent.EXTRA_SUBJECT,"ITEM");
+                intent.putExtra(Intent.EXTRA_TEXT,shareView);
+                mContext.startActivity(intent.createChooser(intent,"Compartir"));
+            }
+        });
         return  new ViewHolder(view);
     }
 
